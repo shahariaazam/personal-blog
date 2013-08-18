57db3a3784233b2e185dd3e3cdd3a3fe7c7e90b6
Shaharia Azam <shaharia.azam@gmail.com>
2013-08-19 00:56:15 +0600

    Few layout tweaked and social media like integration as bootstrap modal

--- assets/js/template.js
+++ assets/js/template.js
@@ -1,18 +1,22 @@
- //Enabling drop down navigation for mobile screen sizes
-
+//Enabling drop down navigation for mobile screen sizes
 $(function () {
-     $("#nav").tinyNav({ header: '-- Navigation --' });
-  });
-
+    $("#nav").tinyNav({ header:'-- Navigation --' });
+});
+$(function () {
+    if ($.cookie('first_visit') !== '0') { //Show modal as Cookie is not set
+        $('#myModal').modal('show');
+        $.cookie('first_visit', '0', {expires:2});  // now set cookie
+    }
+})
 //Adjusting the sub-menu positions so they are centered
-document.onreadystatechange = function() {
+document.onreadystatechange = function () {
     if (document.readyState === 'complete') {
-	 	$("ul.ww-nav ul.dropdown-menu").each(function(){
-	 		parentWidth = $(this).parent().innerWidth();
-	 		menuWidth = $(this).innerWidth();
-	 		margin = (parentWidth / 2 ) - (menuWidth / 2);
-	 		margin = margin + "px";
-	 		$(this).css("margin-left", margin);
-	 	});
- 	}
+        $("ul.ww-nav ul.dropdown-menu").each(function () {
+            parentWidth = $(this).parent().innerWidth();
+            menuWidth = $(this).innerWidth();
+            margin = (parentWidth / 2 ) - (menuWidth / 2);
+            margin = margin + "px";
+            $(this).css("margin-left", margin);
+        });
+    }
 }
